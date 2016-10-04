6260c549c49f238b35ee0fb1677c35f676db47e4
CatalinaSandu <sanducatalina10@yahoo.com>
2016-10-04 13:27:02 +0300

    profile users db

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
  def facebook
+  omniauth = request.env["omniauth.auth"]
+  session[:fb_token] = omniauth["credentials"]["token"] if omniauth['provider'] == 'facebook'
 
-   omniauth = request.env["omniauth.auth"]
-   session[:fb_token] = omniauth["credentials"]["token"] if omniauth['provider'] == 'facebook'
-
-   @user = User.from_omniauh(request.env["omniauth.auth"])
-   if @user.persisted?
+  @user = User.from_omniauh(request.env["omniauth.auth"])
+  if @user.persisted?
     sign_in_and_redirect @user, :event => :authentication
   else
     session["devise.facebook_data"] = request.env["omniauth.auth"]
