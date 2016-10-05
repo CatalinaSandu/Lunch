b38e7aab2a691c37a04bee5da108d60a87e5f11d
CatalinaSandu <sanducatalina10@yahoo.com>
2016-10-05 11:59:20 +0300

    images for new menu & facebook_logout(partial)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,10 +12,12 @@ class ApplicationController < ActionController::Base
   end
 
   def facebook_logout
-    split_token = session[:fb_token].split("|")
-    fb_api_key = split_token[0]
-    fb_session_key = split_token[1]
-    redirect_to "http://www.facebook.com/logout.php?api_key=#{fb_api_key}&session_key=#{fb_session_key}&confirm=1&next=#{destroy_user_session_url}";
+    # split_token = session[:fb_token].split("|")
+    # fb_api_key = split_token[0]
+    # fb_session_key = split_token[1]
+    fb_api_key = '1718333038491869'
+    fb_session_key = session[:fb_token]
+    redirect_to "http://www.facebook.com/logout.php?api_key=#{fb_api_key}&session_key=#{fb_session_key}&confirm=1";
     session.clear
   end
 
